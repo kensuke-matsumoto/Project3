d9765d5d298e711bc99bfed1a7511af0d1e81396
Kensuke Matsumoto <kensuke.matsumoto@woven-planet.global>
2021-11-10 21:01:37 +0900

    enermy controller added

--- RPG/Assets/RPG/Script/Enermy/BanditBehaiviour.cs
+++ RPG/Assets/RPG/Script/Enermy/BanditBehaiviour.cs
@@ -10,13 +10,23 @@ namespace RPG
       public float detectionRadius = 10.0f;
       public float detectionAngle = 90.0f; 
       public float timeToStopPursuit = 2.0f;
+      public float timeToWaitOnPursuit = 2.0f;
       private PlayerController m_Target;
       private NavMeshAgent m_NavMeshAgent; 
+      private Animator m_Animator;
       private float m_TimeSinceLostTarget = 0;
+      private Vector3 m_OriginPosition;
+       private readonly int m_HashInPursuit = Animator.StringToHash("InPursuit");
+       private readonly int m_HashNearBase = Animator.StringToHash("NearBase");    
 
         private void Awake()
         {
           m_NavMeshAgent = GetComponent<NavMeshAgent>();
+          m_Animator = GetComponent<Animator>();        
+          m_OriginPosition = transform.position;
+          Debug.Log("Hello !");
+          
+
         }
         private void Update()
         {
@@ -32,14 +42,19 @@ namespace RPG
           else
           {  
             m_NavMeshAgent.SetDestination(m_Target.transform.position);
+            m_Animator.SetBool(m_HashInPursuit, true);
+
             if(target == null) 
             {
               m_TimeSinceLostTarget += Time.deltaTime;
               if(m_TimeSinceLostTarget >= timeToStopPursuit)
               {
                 m_Target = null;
-                Debug.Log("stopping enermy !!");
-                
+                m_NavMeshAgent.isStopped = true;
+                m_Animator.SetBool(m_HashInPursuit, false);
+                StartCoroutine(WaitOnPursuit());
+             
+
 
               }
             } 
@@ -50,9 +65,22 @@ namespace RPG
             }     
            
           }
+          Vector3 toBase = m_OriginPosition - transform.position;
+          toBase.y = 0;
+          m_Animator.SetBool(m_HashNearBase, toBase.magnitude < 0.01f);
           
 
         }
+        private IEnumerator WaitOnPursuit()
+        {
+          yield return new WaitForSeconds(timeToWaitOnPursuit);
+          m_NavMeshAgent.isStopped = false;
+          m_NavMeshAgent.SetDestination(m_OriginPosition);
+
+
+
+        }
+
         private PlayerController LookForPlayer()
         {
           if(PlayerController.Instance == null)
@@ -62,10 +90,7 @@ namespace RPG
           Vector3 enermyPosition = transform.position;
           Vector3 toPlayer = PlayerController.Instance.transform.position - enermyPosition; // target(player) - this object(enermy) in this case 
           toPlayer.y = 0;
-          //Debug.Log("toPlayer vector" + toPlayer);
-          //Debug.Log("transform.forward" + transform.forward);
-          //Debug.Log("Dot value" + Vector3.Dot(toPlayer.normalized, transform.forward));
-          //Debug.Log("Cos value" + Mathf.Cos(detectionAngle * 0.5f * Mathf.Deg2Rad));
+         
 
           if(toPlayer.magnitude <= detectionRadius)
           {
